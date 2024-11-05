b8f201730f421e1e18f5736c9f37b18b531adda8
Carson Haskell <carson.haskell@gmail.com>
2024-11-04 16:39:52 -0800

    initial author routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 const app = express()
 
 app.set('view engine', 'ejs')
@@ -11,6 +12,7 @@ app.set('layout', 'layouts/layout')
 
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 // DATABASE
 const mongoose = require('mongoose')
@@ -26,6 +28,9 @@ db.once('open', () => {
 // ROUTES
 const indexRouter = require('./routes/index')
 app.use('/', indexRouter)
+
+const authorsRouter = require('./routes/authors')
+app.use('/authors', authorsRouter)
 
 // SERVER
 app.listen(process.env.PORT || 3000, () =>
